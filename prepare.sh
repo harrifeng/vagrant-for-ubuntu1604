a6d8c059af1d5e3fb47adb763d0d23db6fe5ef00
Harri Feng <harrifeng@gmail.com>
2020-01-18 17:51:04 +0800

    Init the project

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+echo "
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+" | sudo tee /etc/apt/sources.list
+
+sudo apt update
+sudo apt install -y git silversearcher-ag lrzsz nload
+
+echo "<----------finishing provision--------------->"
